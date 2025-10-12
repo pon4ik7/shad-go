c388f34860e09b679655692964bdb70e84c57a3d
Rolan <muliukinrolan@mail.ru>
2025-10-12 19:10:40 +0300

    feat Solve urlFetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,27 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	args := os.Args[1:]
+	for _, arg := range args {
+		resp, err := http.Get(arg)
+		if err != nil {
+			fmt.Println(err)
+			panic(1)
+		}
+		defer resp.Body.Close()
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			panic(1)
+		}
+		fmt.Println(string(data))
+	}
 }
