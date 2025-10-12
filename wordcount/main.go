d0c3582a43067d08a777d42cd41fe1740d21cd2a
Rolan <muliukinrolan@mail.ru>
2025-10-12 18:39:02 +0300

    feat Solve wordCount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,32 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	mp := make(map[string]int)
+	for _, name := range args {
+		data, err := os.ReadFile(name)
+		check(err)
+		words := strings.Split(string(data), "\n")
+		for _, word := range words {
+			mp[word]++
+		}
+	}
+	for k, v := range mp {
+		if v >= 2 {
+			fmt.Printf("%d\t%s\n", v, k)
+		}
+	}
 }
