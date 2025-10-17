5f8bd0bb15f807a711d20f7a91e11b4e9ee1abad
Rolan <muliukinrolan@mail.ru>
2025-10-17 15:10:27 +0300

    feat Solve hogwarts

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -2,6 +2,31 @@
 
 package hogwarts
 
-func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+func GetCourseList(prereqs map[string][]string) (ans []string) {
+	ans = make([]string, 0)
+
+	visited := make(map[string]bool)
+	lived := make(map[string]bool)
+
+	var topSort func(string)
+	topSort = func(s string) {
+		visited[s] = true
+		for _, next := range prereqs[s] {
+			if visited[next] && !lived[next] {
+				panic("have a cycle")
+			}
+			if !visited[next] {
+				topSort(next)
+			}
+		}
+		ans = append(ans, s)
+		lived[s] = true
+	}
+
+	for course := range prereqs {
+		if !visited[course] {
+			topSort(course)
+		}
+	}
+	return
 }
