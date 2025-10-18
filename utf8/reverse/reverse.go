d472f393d3e27ca8091508b80352870a831fc5c9
Rolan <muliukinrolan@mail.ru>
2025-10-18 10:16:26 +0300

    feat Solve utf8

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,18 @@
 
 package reverse
 
+import "unicode/utf8"
+
 func Reverse(input string) string {
-	return ""
+	out := make([]byte, 0, len(input))
+	for len(input) > 0 {
+		r, n := utf8.DecodeLastRuneInString(input)
+		if r == utf8.RuneError {
+			out = append(out, []byte(string(utf8.RuneError))...)
+		} else {
+			out = append(out, input[(len(input)-n):]...)
+		}
+		input = input[:len(input)-n]
+	}
+	return string(out)
 }
--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,32 @@
 
 package spacecollapse
 
+import (
+	"unicode/utf8"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	out := make([]byte, 0, len(input))
+	check := false
+	for len(input) > 0 {
+		err, n := utf8.DecodeRuneInString(input)
+		if err == utf8.RuneError {
+			out = append(out, []byte(string(utf8.RuneError))...)
+			check = false
+		} else {
+			if input[:n] != " " && input[:n] != "\t" && input[:n] != "\n" && input[:n] != "\r" {
+				check = false
+			} else {
+				if !check {
+					out = append(out, []byte(" ")...)
+					check = true
+				}
+			}
+			if !check {
+				out = append(out, input[:n]...)
+			}
+		}
+		input = input[n:]
+	}
+	return string(out)
 }
